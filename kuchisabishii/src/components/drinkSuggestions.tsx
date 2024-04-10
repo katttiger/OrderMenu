bdcde47ad1a9c4bb561bba0f0db56c9862e507f0
katttiger <katttiger@gmail.com>
2024-04-10 16:53:18 +0200

    Started working on drinksuggestions.

---
+++ kuchisabishii/src/components/drinkSuggestions.tsx
@@ -0,0 +1,22 @@
+//Importera dryckernas egenskaper
+
+// import { DrinkCard } from "./drinkcard"
+
+
+export const DrinkSuggestions = () => {
+    return (
+        <>
+            {/* Use isHidiing
+            IF addedToCart=> isHiding===false
+            ELSE isHiding===true
+            */}
+            <div>
+                {/*                     
+            <DrinkCard/>
+            <DrinkCard/>
+            <DrinkCard/> */}
+            </div>
+        </>
+    )
+
+}
--- kuchisabishii/src/components/foodcard.tsx
+++ kuchisabishii/src/components/foodcard.tsx
@@ -1,8 +1,13 @@
 import styles from "./foodcard.module.css";
 import { ProductCardHeader } from "./productcardHeader";
 import { CartManager } from "./cartmanager";
+import { useContext } from "react";
+import { CartContext } from "../App";
+import { DrinkSuggestions } from "./drinkSuggestions";
 
 export const Foodcard = ({ food }: { food: Food }) => {
+  const {cart} = useContext(CartContext);
+  const isItemInCart = cart.some((item) => item.id === food._id);
   return (
     <>
       <div className={styles.productCard} style={{ backgroundImage: `url(${food.imageUrl})` }} >
@@ -10,9 +15,15 @@ export const Foodcard = ({ food }: { food: Food }) => {
           title={food.title}
           description={food.description}
           price={food.price}
+          
         />
         <CartManager id={food._id} title={food.title} price={food.price} />
       </div>
+      <div style={{ visibility: isItemInCart ? "visible" : "hidden" }}>
+        <DrinkSuggestions />
+      </div>
+
+         
     </>
   );
 };
