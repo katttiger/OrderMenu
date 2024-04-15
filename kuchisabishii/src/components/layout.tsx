c23db8f02a88f08cf06c0e1bb9cc4d2b1fa41a67
Yanays Rodriguez Puentes <yanays.rodriguez.puentes@iths.se>
2024-04-15 16:23:44 +0200

    trying to fix bar positions

--- kuchisabishii/src/components/layout.tsx
+++ kuchisabishii/src/components/layout.tsx
@@ -5,13 +5,8 @@ export const Layout = () => {
     return (
         <>
             <div className="container-fluid">
-                <div className="sticky-top">
-                    <Header />
-                </div>
-                <div>
-                    <Outlet />
-                </div>
-
+                <Header />
+                <Outlet />
             </div>
         </>
     )
