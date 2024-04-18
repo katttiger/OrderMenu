38bd5c6142c7aa40e83c2b11279fc920e7c4ee18
Anton Eneroth <anton.eneroth@gmail.com>
2024-04-18 13:40:42 +0200

    Changed text in Navbars Navlink from Food to Mains

--- kuchisabishii/src/components/navbar.tsx
+++ kuchisabishii/src/components/navbar.tsx
@@ -1,12 +1,28 @@
-import { NavLink } from "react-router-dom"
-import styles from './navbar.module.css'
+import { NavLink } from "react-router-dom";
+import styles from "./navbar.module.css";
 export const Navbar = () => {
-
-    return (
-        <div className={` d-flex justify-content-between text-dark ${styles.links}`}>
-            <NavLink to="/food" className={({ isActive }) => isActive ? styles.active : ""}>Food</NavLink >
-            <NavLink to="/sides" className={({ isActive }) => isActive ? styles.active : ""}>Sides</NavLink >
-            <NavLink to="/drinks" className={({ isActive }) => isActive ? styles.active : ""}>Cocktails</NavLink >
-        </div>
-    )
-}
+  return (
+    <div
+      className={` d-flex justify-content-between text-dark ${styles.links}`}
+    >
+      <NavLink
+        to="/food"
+        className={({ isActive }) => (isActive ? styles.active : "")}
+      >
+        Mains
+      </NavLink>
+      <NavLink
+        to="/sides"
+        className={({ isActive }) => (isActive ? styles.active : "")}
+      >
+        Sides
+      </NavLink>
+      <NavLink
+        to="/drinks"
+        className={({ isActive }) => (isActive ? styles.active : "")}
+      >
+        Cocktails
+      </NavLink>
+    </div>
+  );
+};
