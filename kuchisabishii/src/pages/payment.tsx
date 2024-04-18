49a63a19ee422ba3f2a1c36188d013666f52619c
SintoriSan <sindrielfarsson@gmail.com>
2024-04-18 11:39:59 +0200

    cart menu

--- kuchisabishii/src/pages/payment.tsx
+++ kuchisabishii/src/pages/payment.tsx
@@ -1,8 +1,8 @@
 export const Payment = () => {
-    return(
-        <>
-            <h1>Order payment</h1>
-            <button className="btn btn-success">Pay</button>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Order payment</h1>
+      <button className="btn btn-success">Pay</button>
+    </>
+  );
+};
