bf28f999c8dd2706abb1b253df4a6528321cf4d6
Yanays Rodriguez Puentes <yanays.rodriguez.puentes@iths.se>
2024-04-24 15:11:26 +0200

    Darker shadow and picture frames in drinkSuggestions component

--- kuchisabishii/src/App.tsx
+++ kuchisabishii/src/App.tsx
@@ -58,13 +58,13 @@ function App() {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [drinksAndCategory, setDrinksAndCategory] = useState<DrinksAndCategory[]>([]);
   const isMounted = useRef(false);
-  const beveragesUrl =
+  const drinksUrl =
     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=";
 
   useEffect(() => {
     if (!isMounted.current) {
       drinkCategories.map((category) => {
-        fetch(beveragesUrl + category)
+        fetch(drinksUrl + category)
           .then((response) => response.json())
           .then((data: Drinks) => {
             const newDrinksAndCategory: DrinksAndCategory = {
