8b31078ff224577ab6dfe225a456ccd56bbfdc4d
katttiger <katttiger@gmail.com>
2024-04-24 12:19:49 +0200

    Removed unnecesary comments.

--- kuchisabishii/src/components/cartbar.tsx
+++ kuchisabishii/src/components/cartbar.tsx
@@ -12,12 +12,6 @@ export const Cartbar = ({ cartTotal }: { cartTotal: number }) => {
           aria-controls="offcanvasBottom"
         >
           <div className="d-flex justify-content-between align-items-baseline px-3">
-            {/* <button className="btn position-relative p-0 mb-2">
-              <i className="fa fa-shopping-cart text-white fs-5 position-absolute top-50 end-50"></i>
-              <span className="badge btn-badge rounded-100 fs-4 text-white m-0 p-0">
-                {cartTotal}
-              </span>
-            </button> */}
             <div className="position-relative">
               <span className={styles.cartTotal}>
                 {cartTotal}
--- kuchisabishii/src/components/header.tsx
+++ kuchisabishii/src/components/header.tsx
@@ -2,21 +2,25 @@ import { Navbar } from "./navbar"
 import styles from "./header.module.css";
 
 export const Header = () => {
+
     return (
         <>
             <div className={styles.Header}>
-                
+
+                {/*Put kanji in header*/}
                 {/* <h5>口寂しい  </h5> */}
+                {/* Fix onClick => navigate to top of main page */}
+
                 <div className={styles.p}>
-                <h1>
-                    Kuchisabishii
-                </h1>
+                    <h1>
+                        Kuchisabishii
+                    </h1>
                     <i>
                         Asian fusion for your lonely mouth
                     </i>
                 </div>
                 <Navbar />
-            </div>
+            </div >
         </>
     )
 }
