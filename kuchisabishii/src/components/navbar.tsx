270390554fe4da0035d0adc4729fd5cb7f8e8493
katttiger <katttiger@gmail.com>
2024-04-25 08:48:56 +0200

    Fixed scroll to top when navigating to new page.

--- kuchisabishii/src/components/navbar.tsx
+++ kuchisabishii/src/components/navbar.tsx
@@ -1,6 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import styles from "./navbar.module.css";
+import { useEffect } from "react";
 export const Navbar = () => {
+  const scrollToTop = () => {
+    const location = useLocation();
+    useEffect(() => {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }, [location])
+  }
+
   return (
     <div className={` d-flex justify-content-between text-dark ${styles.links}`}>
       <NavLink
@@ -10,7 +18,7 @@ export const Navbar = () => {
       >
         <h6>Mains</h6>
       </NavLink>
-      <NavLink to="/sides" className={({ isActive }) => (isActive ? styles.active : "")}>
+      <NavLink to="/sides" className={({ isActive }) => (isActive ? styles.active : "")} >
         <h6>Sides</h6>
       </NavLink>
       <NavLink to="/drinks" className={({ isActive }) => (isActive ? styles.active : "")}>
