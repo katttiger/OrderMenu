5193de73aeabffb0d7a3edc19b340bbb2ee1d3ed
LethalGerard <lethal_kim@hotmail.com>
2024-05-06 20:40:07 +0200

    Co-authored-by: katttiger <katttiger@users.noreply.github.com> Co-authored-by: YanaysIT <YanaysIT@users.noreply.github.com> Co-authored-by: Andreas Lindell <AndreasELindell@users.noreply.github.com> Co-authored-by: Sindri Elfarsson <Sintorisan@users.noreply.github.com> Co-authored-by: Anronton <Anronton@users.noreply.github.com> Co-authored-by: aanthony-lai <aanthony-lai@users.noreply.github.com>

--- kuchisabishii/src/components/chatbot.tsx
+++ kuchisabishii/src/components/chatbot.tsx
@@ -1,36 +1,54 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./chatbot.module.css";
 
+type ChatMessage = {
+  message: string;
+  author: string;
+};
+
 export const Chatbot = () => {
-  const [message, setMessage] = useState<string>();
+  const [message, setMessage] = useState<string>("");
+  const [chatHistory, setChatHistory] = useState<ChatMessage[]>([]);
 
-  const [chatHistory, setChatHistory] = useState<string[]>([]);
-
-  const sendquestion = (question: string) => {
-    setChatHistory(() => [...chatHistory, question]);
+  const sendquestion = () => {
+    let chatMessage: ChatMessage = {
+      message: message,
+      author: "user",
+    };
+    setChatHistory((chat) => [...chat, chatMessage]);
+    console.log(message);
     sendQuestionToChatApi();
+    setMessage("");
+    
   };
 
-  const sendQuestionToChatApi = () => {
-    useEffect(() => {
-      fetch("http://localhost:7123/api/chat", {
-        method: "POST",
-        body: JSON.stringify(chatHistory),
-      })
-        .then((res) => res.json())
-        .then((data) => setChatHistory(() => [...chatHistory, data]))
-        .catch((err) => console.log(err));
-    }, [chatHistory]);
+  const sendQuestionToChatApi = async () => {
+    await fetch("https://localhost:7123/api/Chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    })
+      .then((res) => res.text())
+      .then((data) =>
+        setChatHistory((chatHistory) => [
+          ...chatHistory,
+          { message: data, author: "system" },
+        ])
+      )
+      .then(() => console.log(chatHistory))
+      .catch((err) => console.log(err));
   };
+
   return (
     <>
-      <div
-        className={styles.chatbot}
+      <i
+        className={`fas fa-regular fa-comments fa-2xl ${styles.chatbot}`}
+        
         data-bs-toggle="offcanvas"
         data-bs-target="#chatBot"
-      >
-        <i className="fas fa-comment-dots"></i>
-      </div>
+      ></i>
 
       <div
         className="offcanvas offcanvas-start"
@@ -43,38 +61,34 @@ export const Chatbot = () => {
           <button
             type="button"
             className="btn-close"
-            data-bs-dismiss="canvas"
+            data-bs-dismiss="offcanvas"
             aria-label="Close"
           ></button>
         </div>
         <div className={styles.offcanvasChatbody}>
-          <p className={styles.chatmessage}>
-            Hello! What can i do for you kind person Hello! What can i do for
-            you kind person
-          </p>
-          <p className={styles.chatmessage}>
-            Hello! What can i do you for kind person
-          </p>
-          <p className={styles.chatmessage}>
-            Hello! What can i do you for kind person Hello! What can i do for
-            you kind person Hello! What can i do for you kind person
-          </p>
-          <p className={styles.chatmessage}>
-            Hello! What can i do you for kind person
-          </p>
-          {chatHistory.map((chatmessage) => (
-            <p className={styles.chatmessage}>{chatmessage}</p>
-          ))}
+          <p className={styles.system}>Hello! What can i do for you?</p>
+          {chatHistory.map((chatmessage) =>
+            chatmessage.author === "user" ? (
+              <p key={chatmessage.message} className={styles.user}>
+                {chatmessage.message}
+              </p>
+            ) : (
+              <p key={chatmessage.message} className={styles.system}>
+                {chatmessage.message}
+              </p>
+            )
+          )}
         </div>
-        <div className="offcanvas-footer">
+        <div id='end-of-chat'></div>
+        <div className={styles.offcanvasfooter}>
           <input
             type="text"
             value={message}
             onChange={(m) => setMessage(m.target.value)}
           />
           <button
-            className="btn btn-success"
-            onClick={() => sendquestion(message)}
+            className={`btn btn-success ${styles.sendmessage}`}
+            onClick={message.length > 0 ? () => sendquestion() : () => {}}
           >
             Send
           </button>
